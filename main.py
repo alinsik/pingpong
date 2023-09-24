57bf56eda5df5db661bce9653dff4ce599811163
Allinsik <uravorobec2009@gmail.com>
2023-09-24 15:10:12 +0300

    limited recret move

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ pygame.display.set_caption("Гра Пінг Понг. Автор: Аліна Б�
 #                     pygame.image.load("..."), 
 #                     (width, height)
 #             )
+
+background_color = (189, 0, 255)
+
+
+
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x,y, speed, size):
         super().__init__()
@@ -35,12 +40,29 @@ class GameSprite(pygame.sprite.Sprite):
 
 class Player(GameSprite):
     def update_r(self):
-        ...
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:    
+            self.rect.y += self.speed
 
     def update_l(self):
-        ...
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:    
+            self.rect.y += self.speed
 
 
+
+ball = GameSprite("ball.png", width/2, height/2, 0, (50,50))
+
+racket_1 = Player("racket.png", 10, height/2-50, 7, (50,200))
+
+racket_2 = Player("racket.png", width-35, height/2-50, 7, (50,200))
+
+speed_x = 5
+speed_y = 5
 game_over = False
 finish = False
 
@@ -48,6 +70,22 @@ while not game_over:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game_over = True
+
+    window.fill(background_color)
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+
+    if ball.rect.y > height-50 or ball.rect.y < 0:
+        speed_y *= -1
+
+    ball.reset()
+
+    racket_1.reset()
+    racket_2.reset()
+
+    racket_1.update_l()
+    racket_2.update_r()
+
 
 
     pygame.display.update()
